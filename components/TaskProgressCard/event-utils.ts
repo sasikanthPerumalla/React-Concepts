3693a6a1faf62fd6052a19a793137b312f922ebf
sasikanthPerumalla <34119726+sasikanthPerumalla@users.noreply.github.com>
2026-08-27 22:08:16 +0530

    Test

---
+++ components/TaskProgressCard/event-utils.ts
@@ -0,0 +1,20 @@
+export type PegaAction = 'openWorkByHandle' | 'openAssignment' | 'showPage';
+
+export function handleEvent(
+  getPConnect: () => any,
+  action: PegaAction,
+  payload: Record<string, any>
+) {
+  const pConn = getPConnect();
+  switch (action) {
+    case 'openWorkByHandle':
+      pConn.getActionsApi().openWorkByHandle(payload.caseID, payload.className);
+      break;
+    case 'openAssignment':
+      pConn.getActionsApi().openAssignment(payload.assignmentID);
+      break;
+    case 'showPage':
+      pConn.getActionsApi().showPage(payload.pageName, payload.className);
+      break;
+  }
+}
